999324f53726a56883784606c751598557bfdcc1
dixit <savaliyadixit045@gmail.com>
2023-11-04 14:26:33 +0530

    Add useAuth hook for Firebase authentication

---
+++ Hooks/useAuth.ts
@@ -0,0 +1,35 @@
+import { auth } from "../firebase/firebase";
+
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+const useAuth = () => {
+  // * Functions
+
+  const login = async (email: string, password: string) => {
+    console.log(email, password);
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error: any) {
+      console.log(error?.code);
+    }
+  };
+
+  return {
+    login,
+    logout,
+  };
+};
+
+export default useAuth;
